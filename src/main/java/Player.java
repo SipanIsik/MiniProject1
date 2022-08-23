95653c7381e14a9f5419d8e03fc24d22e46df8c9
HaidarE <98402448+HaidarE@users.noreply.github.com>
2022-08-23 13:59:27 +0200

    "Player class v.1 klart"

--- src/main/java/Player.java
+++ src/main/java/Player.java
@@ -1,2 +1,63 @@
 public class Player {
+    private int x;
+    private int y;
+    private int PreviousX;
+    private int PreviousY;
+    private char Symbol;
+
+    public Player(int x, int y, char symbol) {
+        this.x = x;
+        this.y = y;
+        this.Symbol = symbol;
+        this.PreviousX=x;
+        this.PreviousY=y;
+    }
+    public int getX() {
+        return x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    public int getPreviousX() {
+        return PreviousX;
+    }
+
+
+    public int getPreviousY() {
+        return PreviousY;
+    }
+
+
+    public char getSymbol() {
+        return Symbol;
+    }
+
+    void moveUp (){
+        PreviousX = x;
+        PreviousY=y;
+        y-=1;
+    } void moveDown (){
+        PreviousX = x;
+        PreviousY=y;
+        y+=1;
+    } void moveLeft (){
+        PreviousX = x;
+        PreviousY=y;
+        x-=1;
+    } void moveRight (){
+        PreviousX=x;
+        PreviousY=y;
+        x+=1;
+    }
+
 }
