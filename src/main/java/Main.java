a7ed7b91cc3bed16c0df8bc02e84cd97fa3159d6
DeryaBaykal <derya.baykal91@gmail.com>
2022-08-23 12:59:02 +0200

    Added getKeyStroke method

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,10 +12,27 @@ import java.util.Random;
 public class Main {
 
     public static void main(String[] args) throws IOException {
+        //start-deklaration
+        boolean continueReadingInput= true;
+        KeyStroke keyStroke= null;
 
        Terminal terminal= createTerminal();
+       KeyType type;
+       Character c;
 
+        while (continueReadingInput){
+            getKeyStroke(terminal);
 
+            c=keyStroke.getCharacter();
+            type=keyStroke.getKeyType();
+
+            if(c==Character.valueOf('q')) {
+               continueReadingInput=false;
+
+            }
+
+            terminal.flush();
+        }
     }
 
 
@@ -24,6 +41,29 @@ public class Main {
         DefaultTerminalFactory terminalFactory = new DefaultTerminalFactory();
         Terminal terminal = terminalFactory.createTerminal();
         terminal.setCursorVisible(false);
+
         return terminal;
     }
+
+    private static KeyStroke getKeyStroke(Terminal terminal){
+        KeyStroke keyStroke;
+        do {
+            Thread.sleep(5);
+            keyStroke = terminal.pollInput();
+        } while (keyStroke == null);
+        return keyStroke;
+    }
+
+    //create monsters --> after merging Monster Class
+
+
+    //create players --> after merging Player class
+
+    /*private static boolean checkRequestToQuit(){
+        boolean continueReadingInput;
+
+        if
+
+        return continueReadingInput;
+    }*/
 }
